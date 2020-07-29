6376d732b2fc13ff66db5ab7827b5c3b749ca5c2
Kevin Katzer <kevinktzr@gmail.com>
2020-07-29 11:13:38 -0300

    Forms
    Signed-off-by: Kevin Katzer <kevinktzr@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,17 @@ export default function Home({allPostsData}) {
                         </li>
                     ))}
                 </ul>
+                <form name="contact" netlify>
+                    <p>
+                        <label>Name <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Email <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
             </section>
         </Layout>
     )
