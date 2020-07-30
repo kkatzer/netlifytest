651de2d40ae3cf7a9b1bf5061d1f846016956ceb
Kevin Katzer <kevinktzr@gmail.com>
2020-07-30 10:33:24 -0300

    Removed extra field
    Signed-off-by: Kevin Katzer <kevinktzr@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,6 @@ export default function Home({allPostsData}) {
                 <form name="contact" action="/success" method="POST" data-netlify="true">
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
-                        <input type="text" name="firstname" id="firstname" />
                         <label htmlFor="yourname">
                             Your Name:
                         </label> <br />
